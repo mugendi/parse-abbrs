1b266b30462cb4aa6091fcbf015b83a0af325f62
Anthony Mugendi <ngurumugz@gmail.com>
2015-09-07 10:33:39 +0300

    added case sensitive abbrs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 var abbreviations=require('./data/abbrs.json')
 
 
 module.exports = function(statement) {
   
-    var keys=Object.keys(abbreviations),
+    // First we replace all non case-sensitive abbreviations
+    var keys=Object.keys(abbreviations.non_sensitive),
         pat=new RegExp( '\\b('+ escapeRegExp(keys.join('|')) +')\\b','ig' ),
         matches=statement.match(pat),
         parsed={
@@ -19,11 +21,27 @@ module.exports = function(statement) {
          //replace
         matches.forEach(function(abbr){
             pat=new RegExp('\\b'+ escapeRegExp(abbr) +'\\b','g');
-            parsed.string.out=parsed.string.out.replace(pat,abbreviations[abbr.toLowerCase()]);
-            parsed.string.annotated=parsed.string.annotated.replace(pat,'{ABBR: '+abbreviations[abbr.toLowerCase()]+'}');
+            parsed.string.out=parsed.string.out.replace(pat,abbreviations.non_sensitive[abbr.toLowerCase()]);
+            parsed.string.annotated=parsed.string.annotated.replace(pat,'{ABBR: '+abbreviations.non_sensitive[abbr.toLowerCase()]+'}');
             parsed.abbrs.push(abbr)
-        });
-    
+        });    
+    }
+
+
+    // Then we deal with the case sensitive abbreviations
+    var keys=Object.keys(abbreviations.sensitive),
+        pat=new RegExp( '\\b('+ escapeRegExp(keys.join('|')) +')\\b','g' ),
+        matches=statement.match(pat);
+
+    // console.log(matches)
+    if(matches){
+         //replace
+        matches.forEach(function(abbr){
+            pat=new RegExp('\\b'+ escapeRegExp(abbr) +'\\b','g');
+            parsed.string.out=parsed.string.out.replace(pat,abbreviations.sensitive[abbr]);
+            parsed.string.annotated=parsed.string.annotated.replace(pat,'{ABBR: '+abbreviations.sensitive[abbr]+'}');
+            parsed.abbrs.push(abbr)
+        });    
     }
 
    
